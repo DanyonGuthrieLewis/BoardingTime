2f0db86febd4e76cfab57acaa39d672bc79ab095
Danyon <Danyongreymoon@gmail.com>
2018-02-23 09:07:23 -0700

    Salt hash works on register.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
-
+var bcrypt = require('bcrypt-nodejs');
 
 var userModel = require('../models/user');
 
+var myHash;
 var User = userModel.User;
 var userSchema = userModel.userSchema;
 
@@ -33,15 +34,29 @@ exports.registerPost = function(req, res) {
         admin: false,
         avatar: req.body.avatar
     });
-    user.save(function (err, user) {
-        if (err) return console.error(err);
-        console.log(req.body.username + ' added');
-    });
-    res.redirect('/register');
+
+    bcrypt.hash(user.pass, null, null, function(err, hash){
+        if (err) {
+            console.log(err);
+            res.redirect('login');
+        } else {
+            user.pass = hash;
+            user.save(function (err, user) {
+                if (err) {
+                    return console.error(err);
+                }
+                else {
+                    console.log(user.username + ' added with pass ' + user.pass);
+                }
+            });
+            res.redirect('/');
+        }
+    });    
 }
 
 exports.loginPost = function(req, res) {
-    res.send('todo');
+    console.log(req.body)
+    res.redirect('/login');
 }
 
 exports.profilePut = function(req, res) {
